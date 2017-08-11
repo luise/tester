6675b1fa3ce999be42575701f9175167dcb8c9c9
Kevin Lin <kevin@quilt.io>
2017-08-11 10:14:47 -0700

    blueprint: Lint all files

--- tester-runner-example.js
+++ tester-runner-example.js
@@ -2,30 +2,31 @@
 // of all paramters to `tester.New`. It uses a floating IP to automatically
 // configure `jenkinsUrl`.
 
-const {createDeployment, Machine} = require("@quilt/quilt");
-var tester = require("./tester.js");
+const { createDeployment, Machine } = require('@quilt/quilt');
+const tester = require('./tester.js');
 
-var deployment = createDeployment();
-var baseMachine = new Machine({provider: "Amazon"});
+const deployment = createDeployment();
+const baseMachine = new Machine({ provider: 'Amazon' });
 
 deployment.deploy(baseMachine.asMaster());
 
-var worker = baseMachine.asWorker();
-worker.floatingIp = "8.8.8.8";
+const worker = baseMachine.asWorker();
+worker.floatingIp = '8.8.8.8';
 deployment.deploy(worker);
 
 deployment.deploy(tester.New({
-    awsAccessKey: "accessKey",
-    awsSecretAccessKey: "secret",
-    gceProjectID: "projectID",
-    gcePrivateKey: "privateKey",
-    gceClientEmail: "email",
-    digitalOceanKey: "key",
-    testingNamespace: "quilt-tester",
-    slackTeam: "quilt-dev",
-    slackChannel: "#testing",
-    slackToken: "secret",
-    // Because of a bug with the `applyTemplate` function, dollar sign literals (`$`) need to be replaced with `$$`.
-    passwordHash: "#jbcrypt:$$2a$$10$$Jcwink6XXoUIp3Ieh.1QR.Mx5idVA7QNLHNcF2jQWhoCA96y5k/jS",
-    jenkinsUrl: "http://" + worker.floatingIp + ":8080"
+  awsAccessKey: 'accessKey',
+  awsSecretAccessKey: 'secret',
+  gceProjectID: 'projectID',
+  gcePrivateKey: 'privateKey',
+  gceClientEmail: 'email',
+  digitalOceanKey: 'key',
+  testingNamespace: 'quilt-tester',
+  slackTeam: 'quilt-dev',
+  slackChannel: '#testing',
+  slackToken: 'secret',
+  // Because of a bug with the `applyTemplate` function, dollar sign literals
+  // (`$`) need to be replaced with `$$`.
+  passwordHash: '#jbcrypt:$$2a$$10$$Jcwink6XXoUIp3Ieh.1QR.Mx5idVA7QNLHNcF2jQWhoCA96y5k/jS',
+  jenkinsUrl: `http://${worker.floatingIp}:8080`,
 }));
--- tester.js
+++ tester.js
@@ -1,4 +1,4 @@
-const {Container, Service, publicInternet} = require("@quilt/quilt");
+const { Container, Service, publicInternet } = require('@quilt/quilt');
 const fs = require('fs');
 const path = require('path');
 
@@ -6,127 +6,130 @@ const path = require('path');
 // UID other than root. To get around this, we upload our files to a staging
 // directory, and copy them into the Jenkins home directory (as the Jenkins
 // user) before starting Jenkins.
-var jenkinsStagingDir = "/tmp/files/"
-
-exports.New = function(opts) {
-    assertRequiredParameters(opts, [
-        "awsAccessKey", "awsSecretAccessKey",
-        "digitalOceanKey",
-        "gceProjectID", "gcePrivateKey", "gceClientEmail",
-        "testingNamespace",
-        "slackTeam", "slackChannel", "slackToken"]);
-
-    var container = new Container("quilt/tester",
-        ["/bin/bash", "-c",
-            "cp -r " + jenkinsStagingDir + ". /var/jenkins_home;" +
-            "/bin/tini -s -- /usr/local/bin/jenkins.sh"]);
-    container.setEnv("AWS_ACCESS_KEY", opts.awsAccessKey);
-    container.setEnv("AWS_SECRET_ACCESS_KEY", opts.awsSecretAccessKey);
-    container.setEnv("TESTING_NAMESPACE", opts.testingNamespace);
-    container.setEnv("TZ", "/usr/share/zoneinfo/America/Los_Angeles");
-    setupFiles(container, opts);
-
-    var jenkins = new Service("jenkins", [container]);
-
-    // Allow inbound connections to the Jenkins web UI.
-    jenkins.allowFrom(publicInternet, 8080);
-
-    // The tests talk to the deployed machines on various ports. We allow them here.
-    publicInternet.allowFrom(jenkins, 22); // Required by `quilt ssh`.
-    publicInternet.allowFrom(jenkins, 80); // Required by network tests.
-    publicInternet.allowFrom(jenkins, 443); // Required by network tests.
-    publicInternet.allowFrom(jenkins, 8000); // Required by network tests.
-    publicInternet.allowFrom(jenkins, 9200); // Required by the elasticsearch test.
-    publicInternet.allowFrom(jenkins, 9000); // Required by the Quilt daemon for API communication.
-    publicInternet.allowFrom(jenkins, 9999); // Required by the Quilt daemon for minion communcation.
-
-    // Allow outbound connections to Git servers. Required by `npm install`.
-    publicInternet.allowFrom(jenkins, 9418);
-
-    return jenkins;
-}
-
-function setupFiles(jenkins, opts) {
-    var files = [];
-
-    files.push(new File(".digitalocean/key", opts.digitalOceanKey));
-
-    var gceConfig = new File(".gce/quilt.json",
-        applyTemplate(readRel("config/gce.json.tmpl"),
-            {gceProjectID: opts.gceProjectID,
-             gcePrivateKey: opts.gcePrivateKey,
-             gceClientEmail: opts.gceClientEmail}));
-    files.push(gceConfig);
-
-    var rootConfig = new File("config.xml", readRel("config/jenkins/root.xml"));
-    files.push(rootConfig);
-
-    var scriptConfig = new File("scriptApproval.xml", readRel("config/jenkins/scriptApproval.xml"));
-    files.push(scriptConfig);
-
-    var goConfig = new File("org.jenkinsci.plugins.golang.GolangBuildWrapper.xml",
-        readRel("config/jenkins/go.xml"));
-    files.push(goConfig);
-
-    var nodeConfig = new File("jenkins.plugins.nodejs.tools.NodeJSInstallation.xml",
-        readRel("config/jenkins/node.xml"));
-    files.push(nodeConfig);
-
-    var quiltTesterConfig = new File("jobs/quilt-tester/config.xml",
-        applyTemplate(readRel("config/jenkins/quilt-tester.xml"),
-            {slackTeam: opts.slackTeam,
-             slackToken: opts.slackToken,
-             slackChannel: opts.slackChannel}));
-    files.push(quiltTesterConfig);
-
-    var checkDepsConfig = new File("jobs/check-dependencies/config.xml",
-        applyTemplate(readRel("config/jenkins/check-dependencies.xml"),
-            {slackTeam: opts.slackTeam,
-             slackToken: opts.slackToken,
-             slackChannel: opts.slackChannel}));
-    files.push(checkDepsConfig);
-
-    if (opts.passwordHash !== undefined) {
-        var adminConfig = new File("users/admin/config.xml",
-            applyTemplate(readRel("config/jenkins/admin.xml"),
-                {passwordHash: opts.passwordHash}));
-        files.push(adminConfig);
-    }
-
-    if (opts.jenkinsUrl !== undefined) {
-        var locConfig = new File("jenkins.model.JenkinsLocationConfiguration.xml",
-            applyTemplate(readRel("config/jenkins/location.xml"),
-                {jenkinsUrl: opts.jenkinsUrl}));
-        files.push(locConfig);
-    }
-
-    files.forEach(function(f) {
-        jenkins.filepathToContent[jenkinsStagingDir + f.path] = f.content
-    });
-}
+const jenkinsStagingDir = '/tmp/files/';
 
 // applyTemplate replaces the keys defined by `vars` with their corresponding
 // values in `template`. A variable is denoted in the template using {{key}}.
-function applyTemplate(template, vars) {
-    for (k in vars) {
-        template = template.replace("{{" + k + "}}", vars[k]);
-    }
-    return template;
+function applyTemplate(templateArg, vars) {
+  let template = templateArg;
+  Object.keys(vars).forEach((k) => {
+    template = template.replace(`{{${k}}}`, vars[k]);
+  });
+  return template;
 }
 
 function assertRequiredParameters(opts, requiredKeys) {
-    requiredKeys.forEach(function(key) {
-        if (opts[key] === undefined) {
-            throw key + " is required"
-        }
-    })
+  requiredKeys.forEach((key) => {
+    if (opts[key] === undefined) {
+      throw new Error(`${key} is required`);
+    }
+  });
 }
 
-function File(path, content) {
-    this.path = path;
-    this.content = content;
+function File(filepath, content) {
+  this.path = filepath;
+  this.content = content;
 }
 
 function readRel(file) {
-    return fs.readFileSync(path.join(__dirname, file), {encoding: 'utf8'});
+  return fs.readFileSync(path.join(__dirname, file), { encoding: 'utf8' });
 }
+
+function setupFiles(opts) {
+  const files = [];
+
+  files.push(new File('.digitalocean/key', opts.digitalOceanKey));
+
+  const gceConfig = new File('.gce/quilt.json',
+    applyTemplate(readRel('config/gce.json.tmpl'),
+      { gceProjectID: opts.gceProjectID,
+        gcePrivateKey: opts.gcePrivateKey,
+        gceClientEmail: opts.gceClientEmail }));
+  files.push(gceConfig);
+
+  const rootConfig = new File('config.xml', readRel('config/jenkins/root.xml'));
+  files.push(rootConfig);
+
+  const scriptConfig = new File('scriptApproval.xml', readRel('config/jenkins/scriptApproval.xml'));
+  files.push(scriptConfig);
+
+  const goConfig = new File('org.jenkinsci.plugins.golang.GolangBuildWrapper.xml',
+    readRel('config/jenkins/go.xml'));
+  files.push(goConfig);
+
+  const nodeConfig = new File('jenkins.plugins.nodejs.tools.NodeJSInstallation.xml',
+    readRel('config/jenkins/node.xml'));
+  files.push(nodeConfig);
+
+  const quiltTesterConfig = new File('jobs/quilt-tester/config.xml',
+    applyTemplate(readRel('config/jenkins/quilt-tester.xml'),
+      { slackTeam: opts.slackTeam,
+        slackToken: opts.slackToken,
+        slackChannel: opts.slackChannel }));
+  files.push(quiltTesterConfig);
+
+  const checkDepsConfig = new File('jobs/check-dependencies/config.xml',
+    applyTemplate(readRel('config/jenkins/check-dependencies.xml'),
+      { slackTeam: opts.slackTeam,
+        slackToken: opts.slackToken,
+        slackChannel: opts.slackChannel }));
+  files.push(checkDepsConfig);
+
+  if (opts.passwordHash !== undefined) {
+    const adminConfig = new File('users/admin/config.xml',
+      applyTemplate(readRel('config/jenkins/admin.xml'),
+        { passwordHash: opts.passwordHash }));
+    files.push(adminConfig);
+  }
+
+  if (opts.jenkinsUrl !== undefined) {
+    const locConfig = new File('jenkins.model.JenkinsLocationConfiguration.xml',
+      applyTemplate(readRel('config/jenkins/location.xml'),
+        { jenkinsUrl: opts.jenkinsUrl }));
+    files.push(locConfig);
+  }
+
+  return files;
+}
+
+exports.New = function New(opts) {
+  assertRequiredParameters(opts, [
+    'awsAccessKey', 'awsSecretAccessKey',
+    'digitalOceanKey',
+    'gceProjectID', 'gcePrivateKey', 'gceClientEmail',
+    'testingNamespace',
+    'slackTeam', 'slackChannel', 'slackToken']);
+
+  const container = new Container('quilt/tester',
+    ['/bin/bash', '-c',
+      `cp -r ${jenkinsStagingDir}. /var/jenkins_home;` +
+            '/bin/tini -s -- /usr/local/bin/jenkins.sh']);
+  container.setEnv('AWS_ACCESS_KEY', opts.awsAccessKey);
+  container.setEnv('AWS_SECRET_ACCESS_KEY', opts.awsSecretAccessKey);
+  container.setEnv('TESTING_NAMESPACE', opts.testingNamespace);
+  container.setEnv('TZ', '/usr/share/zoneinfo/America/Los_Angeles');
+
+  const files = setupFiles(opts);
+  files.forEach((f) => {
+    container.filepathToContent[jenkinsStagingDir + f.path] = f.content;
+  });
+
+  const jenkins = new Service('jenkins', [container]);
+
+  // Allow inbound connections to the Jenkins web UI.
+  jenkins.allowFrom(publicInternet, 8080);
+
+  // The tests talk to the deployed machines on various ports. We allow them here.
+  publicInternet.allowFrom(jenkins, 22); // Required by `quilt ssh`.
+  publicInternet.allowFrom(jenkins, 80); // Required by network tests.
+  publicInternet.allowFrom(jenkins, 443); // Required by network tests.
+  publicInternet.allowFrom(jenkins, 8000); // Required by network tests.
+  publicInternet.allowFrom(jenkins, 9200); // Required by the elasticsearch test.
+  publicInternet.allowFrom(jenkins, 9000); // Required by the Quilt daemon for API communication.
+  publicInternet.allowFrom(jenkins, 9999); // Required by the Quilt daemon for minion communcation.
+
+  // Allow outbound connections to Git servers. Required by `npm install`.
+  publicInternet.allowFrom(jenkins, 9418);
+
+  return jenkins;
+};
